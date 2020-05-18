605275350a008f2e0b57fef51bec1a6a8a7276d4
Israel Lana <israellana_@hotmail.com>
2020-05-18 18:23:29 -0300

    Updates

---
+++ precache-manifest.f663d298542576ff0d8a0f1f39645de9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77bb3582da5568a53b45836a25a07f37",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "2bbbf3e7284b78424846",
+    "url": "/monsters-roledex/static/css/main.b61744ca.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-roledex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbbf3e7284b78424846",
+    "url": "/monsters-roledex/static/js/main.37a87650.chunk.js"
+  },
+  {
+    "revision": "6d5b5ec4dac54096337c",
+    "url": "/monsters-roledex/static/js/runtime-main.f85c0cb9.js"
+  }
+]);
